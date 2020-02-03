7656a9444f77ee40bc17389792fc1badf58395c8
= <=>
2020-02-03 11:57:22 +0700

    Added

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -107,3 +107,15 @@ class LoginViewSet(viewsets.ViewSet):
         """Use the ObtainAuthToken APIView to validata and create token."""
 
         return ObtainAuthToken().post(request)
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items."""
+
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user."""
+
+        serializer.save(user_profile=self.request.user)
