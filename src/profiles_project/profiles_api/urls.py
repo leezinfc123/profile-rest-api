3c31fd465a77419f58b99b22a859c8dfdb1eeaa8
= <=>
2020-02-03 10:41:41 +0700

    Added login API.

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -6,10 +6,10 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register('hello-viewset',views.HelloViewSet,base_name = 'hello-viewset')
 router.register('profile',views.UserProfileViewSet)
+router.register('login',views.LoginViewSet,base_name = 'login')
 
 from rest_framework.routers import DefaultRouter
 urlpatterns = [
     url(r'^hello-view/', views.HelloApiView.as_view()),
     url(r'', include(router.urls))
 ]
- 
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -5,6 +5,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
 
 from . import serializers
 from . import models
@@ -92,3 +95,15 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.Userprofile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name','email',)
+
+class LoginViewSet(viewsets.ViewSet):
+    """Check email and password and return an auth Token."""
+
+    serializer_class = AuthTokenSerializer
+
+    def create(self, request):
+        """Use the ObtainAuthToken APIView to validata and create token."""
+
+        return ObtainAuthToken().post(request)
